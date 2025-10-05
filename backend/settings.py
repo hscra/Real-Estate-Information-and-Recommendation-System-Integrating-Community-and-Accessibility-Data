d5e9bbe0fb7ebb45c93aa678d155aa69c0aa22b2
AussieK <yallsung85@gmail.com>
2025-10-05 20:21:06 +0200

    modify schema to latest_listings

--- backend/settings.py
+++ backend/settings.py
@@ -2,15 +2,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import Field, AliasChoices
 
 class Settings(BaseSettings):
-	# previous version #
-    # DATABASE_URL: str = 
-	# SCHEMA: str = "realestate"
-	# VIEW_OR_TABLE: str = "fact_listings"
-    
-    # Updated
     DATABASE_URL: str
     DEBUG: bool = False
-    # Accept either DB_SCHEMA or SCHEMA; default to "realestate"
     SCHEMA: str = Field(
         default="realestate",
         validation_alias=AliasChoices("DB_SCHEMA", "SCHEMA"),
@@ -22,13 +15,11 @@ class Settings(BaseSettings):
         validation_alias=AliasChoices("VIEW_OR_TABLE", "TABLE", "VIEW"),
     )
 
-
-
     # load from .env automatically
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
-        extra="ignore",   # ignore unknown env vars
+        extra="ignore",   
     )
 
 
